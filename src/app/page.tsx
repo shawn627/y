0851468d3ce71fa760a18ef621801801d6b2616f
Shawn Liew <shawn@epik-solutions.com>
2025-07-25 13:25:05 +0800

    change to data-auth-url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import TelegramLoginWidget from "./TelegramLoginWidget";
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <div>
-      <h1>Login with Telegram</h1>
-      <TelegramLoginWidget />
+      <h1>Telegram Login</h1>
+
+      {/* Telegram Login Widget */}
+      <Script src="https://telegram.org/js/telegram-widget.js?7"
+        data-telegram-login="whitelabel_bobolive_bot"  // your bot username
+        data-size="large"
+        data-userpic="true"
+        data-request-access="write"
+        data-auth-url="localhost:3000/auth/telegram"
+        async
+      />
     </div>
   )
 }

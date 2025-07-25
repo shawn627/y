cf06c2e2101dee47c5d4150c9cede325ba7516a4
Shawn Liew <shawn@epik-solutions.com>
2025-07-25 13:55:39 +0800

    Config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' https://telegram.org; frame-src https://oauth.telegram.org",
+          },
+        ],
+      },
+    ]
+  },
+}
 
 export default nextConfig;

7976068a89822e00d466eb50237c276ac2d0eb83
Shawn Liew <shawn@epik-solutions.com>
2025-07-28 11:23:26 +0800

    a

--- src/app/TelegramLogin.tsx
+++ src/app/TelegramLogin.tsx
@@ -1,11 +1,11 @@
 export default function TelegramLogin() {
-    const TelegramBotName = "whitelabelbobolivebot"
+    const TelegramBotName = "whitelabel_bobolive_bot"
     const TelegramBotOrigin = "https://y-tan-five.vercel.app"
     return (
       <div>
         <h1>Login via Telegram</h1>
         <iframe
-          src={`https://oauth.telegram.org/embed/${TelegramBotName}?origin=${TelegramBotOrigin}`}
+          src={`https://oauth.telegram.org/embed/${TelegramBotName}?origin=${TelegramBotOrigin}&embed=1&request_access=write&size=large`}
           width="300"
           height="50"
           frameBorder="0"

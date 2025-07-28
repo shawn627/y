a1eb83ecfe192dde4e00509cf947deaf358726b6
Shawn Liew <shawn@epik-solutions.com>
2025-07-28 14:26:57 +0800

    remove config & telegram commented

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,4 @@
 const nextConfig = {
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/(.*)",
-  //       headers: [
-  //         {
-  //           key: "Content-Security-Policy",
-  //           value:
-  //             "default-src 'self'; " +
-  //             "script-src 'self' https://telegram.org https://oauth.telegram.org 'unsafe-inline' 'unsafe-eval'; " +
-  //             "frame-src https://oauth.telegram.org; " +
-  //             "connect-src 'self'; " +
-  //             "img-src 'self' data: https://telegram.org https://oauth.telegram.org; " +
-  //             "style-src 'self' 'unsafe-inline';",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = nextConfig;
--- src/app/TelegramLogin.tsx
+++ src/app/TelegramLogin.tsx
@@ -1,6 +1,4 @@
-"use client"
 import { LoginButton } from "@telegram-auth/react";
-import { useEffect } from "react";
 
 export default function TelegramLogin() {
   return (
@@ -13,29 +11,5 @@ export default function TelegramLogin() {
     lang="en"
     />
   )
-    // useEffect(() => {
-    //     const handleTelegramLogin = (event: MessageEvent) => {
-    //       if (event.origin !== "https://oauth.telegram.org") return;
-    //       console.log("Telegram data received:", event.data);
-    //     };
-    //     window.addEventListener("message", handleTelegramLogin);
-    //     return () => window.removeEventListener("message", handleTelegramLogin);
-    //   }, []);
-
-      
-    // const TelegramBotName = "whitelabel_bobolive_bot"
-    // const TelegramBotOrigin = "https://y-tan-five.vercel.app"
-    // return (
-    //   <div>
-    //     <h1>Login via Telegram</h1>
-    //     <iframe
-    //       src={`https://oauth.telegram.org/embed/${TelegramBotName}?origin=${TelegramBotOrigin}&embed=1&request_access=write&size=large`}
-    //       width="300"
-    //       height="50"
-    //       frameBorder="0"
-    //       sandbox="allow-scripts allow-same-origin allow-popups allow-forms"
-    //     ></iframe>
-    //   </div>
-    // )
-  }
+}
   

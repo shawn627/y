4e83050a809dea9227cece83c7524571a793d2df
Shawn Liew <shawn@epik-solutions.com>
2025-07-28 15:46:15 +0800

    Google Test

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,17 @@
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'unsafe-none',
+                    },
+                ],
+            },
+        ]
+    },
 };
 
 module.exports = nextConfig;
--- src/app/GoogleLogin.tsx
+++ src/app/GoogleLogin.tsx
@@ -6,6 +6,12 @@ export default function GoogleLoginButton() {
         <GoogleLogin
             onSuccess={credentialResponse => {
                 console.log(credentialResponse);
+                const response = fetch('http://localhost:3000/auth/third-party/google', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(credentialResponse),
+                })
+                console.log(response)
             }}
             onError={() => {
                 console.log('Login Failed');

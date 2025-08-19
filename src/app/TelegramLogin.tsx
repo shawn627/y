b2590fab6bb71bc3f0b234ca04467bc0dcbf6390
Shawn Liew <shawn@epik-solutions.com>
2025-08-19 17:31:29 +0800

    login

--- src/app/TelegramLogin.tsx
+++ src/app/TelegramLogin.tsx
@@ -4,7 +4,7 @@ import { LoginButton } from "@telegram-auth/react";
 export default function TelegramLogin() {
   return (
     <LoginButton
-    botUsername="whitelabel_bobolive_bot"
+    botUsername="test_for_fun_1_bot"
     onAuthCallback={(data) => {
       console.log(data);
       throw new Error("Not implemented");

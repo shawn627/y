9357c5a350738835312df6db621e36539d0ee425
Shawn Liew <shawn@epik-solutions.com>
2025-08-20 12:25:02 +0800

    o

--- src/app/TelegramLogin.tsx
+++ src/app/TelegramLogin.tsx
@@ -16,12 +16,14 @@ export default function TelegramLogin() {
   return (
     <LoginButton
     botUsername="test_for_fun_1_bot"
-    onAuthCallback={async (data: any) => {
+    onAuthCallback={async (data: TelegramData) => {
   
       console.log(data);
+      const reqObj: Record<string, any> = { ...data };
+
       const response = await fetch("http://localhost:3000/auth/telegram/request", {
         method: "POST",
-        body: JSON.stringify(data),
+        body: JSON.stringify(reqObj),
       });
       const result = await response.json();
       console.log(result);
